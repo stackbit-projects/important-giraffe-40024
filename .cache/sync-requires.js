1f24c9fc19cced120e39543fc1afea8d04285d14
Stackbit <projects@stackbit.com>
2021-03-06 22:23:01 +0000

    initial commit by Stackbit

---
+++ .cache/sync-requires.js
@@ -0,0 +1,17 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("E:\\WDUO\\Campbell Immigration Project\\Abstract\\abstract\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("E:\\WDUO\\Campbell Immigration Project\\Abstract\\abstract\\src\\pages\\404.js"))),
+  "component---src-pages-creative-2-index-js": hot(preferDefault(require("E:\\WDUO\\Campbell Immigration Project\\Abstract\\abstract\\src\\pages\\creative-2\\index.js"))),
+  "component---src-pages-creative-video-1-index-js": hot(preferDefault(require("E:\\WDUO\\Campbell Immigration Project\\Abstract\\abstract\\src\\pages\\creative-video-1\\index.js"))),
+  "component---src-pages-creative-video-2-index-js": hot(preferDefault(require("E:\\WDUO\\Campbell Immigration Project\\Abstract\\abstract\\src\\pages\\creative-video-2\\index.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("E:\\WDUO\\Campbell Immigration Project\\Abstract\\abstract\\src\\pages\\index.js"))),
+  "component---src-pages-personal-index-js": hot(preferDefault(require("E:\\WDUO\\Campbell Immigration Project\\Abstract\\abstract\\src\\pages\\personal\\index.js"))),
+  "component---src-pages-personal-video-index-js": hot(preferDefault(require("E:\\WDUO\\Campbell Immigration Project\\Abstract\\abstract\\src\\pages\\personal-video\\index.js")))
+}
+
